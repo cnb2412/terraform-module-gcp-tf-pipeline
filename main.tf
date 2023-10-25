5a43a93e6605c986232462c86a7a3de007945a36
Christian Niephaus <christian@cnb24.de>
2023-10-25 23:13:47 +0200

    storage bucket renaming

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 resource "google_sourcerepo_repository" "my-repo" {
-  project = var.repo_project_id
-  name = var.repo_name
+  project = length(var.repo_project_id) > 0 ? var.repo_project_id : var.project_id
+  name = "${var.resource_prefix}-repo"
 }
 
 resource "google_sourcerepo_repository_iam_member" "editors" {
   count   = length(var.repo_writers)
-  project = var.repo_project_id
+  project = length(var.repo_project_id) > 0 ? var.repo_project_id : var.project_id
   repository = google_sourcerepo_repository.my-repo.name
   role    = "roles/source.writer"
   member  = element(var.repo_writers, count.index)
+}
+
+resource "google_storage_bucket" "tf-state-bucket" {
+    project = length(var.storage_project_id) > 0 ? var.repo_project_id : var.project_id
+    name          = "${var.resource_prefix}-tfstate-storage"
+    location      = var.storage_bucket_location
+    versioning {
+        enabled = true
+    }
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,20 @@
-variable "repo_project_id" {
-  description = "The ID of the project where the repo will be created"
+variable "project_id" {
+  description = <<EOF
+    "The ID of the project where all required ressources will be created. 
+    If configured repo_project_id, storage_project_id should not be configured."
+  EOF
   type        = string
+  default = ""
 }
 
-variable "repo_name" {
-  description = "The name for the source repo"
+variable "repo_project_id" {
+  description = "The ID of the project where the repo will be created."
+  type        = string
+  default = ""
+}
+
+variable "resource_prefix" {
+  description = "The name for the resources. A resource type postfix is appended to the individual ressources."
   type        = string
 }
 
@@ -12,4 +22,16 @@ variable "repo_writers" {
   description = "Optional list of IAM-format members to set as source repo writer."
   type        = list(string)
   default     = []
+}
+
+variable "storage_project_id" {
+  description = "The ID of the project where the repo will be created"
+  type        = string
+  default = ""
+}
+
+variable "storage_bucket_location" {
+  description = "The location of the TF state bucket."
+  type        = string
+  default = "EUROPE-WEST3"
 }
