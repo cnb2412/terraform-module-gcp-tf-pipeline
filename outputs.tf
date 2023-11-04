e836e03c19d849aec34661442de9f66b4f11de98
Christian Niephaus <christian@cnb24.de>
2023-11-04 17:34:13 +0100

    add pro sa output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "test_env_service_account_email" {
   value = module.service-account-test[0].email
 }
+output "prod_env_service_account_email" {
+  value = module.service-account-prod[0].email
+}
